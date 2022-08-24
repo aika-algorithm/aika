9f23019a478180e3644dd34e1b1e53baa252b598
lukas <lukas.molzberger@gmx.de>
2022-08-24 07:14:02 +0200

    Revert "added documentHasBeenCleared flag to document"
    This reverts commit 68226cc02d608d2db84f7a2ba04a1a5c065314d0.

--- src/main/java/network/aika/Document.java
+++ src/main/java/network/aika/Document.java
@@ -68,7 +68,6 @@ public class Document implements Comparable<Document> {
 
     private Model model;
     Integer threadId;
-    boolean documentHasBeenCleared = false;
 
     long startTime;
 
@@ -216,9 +215,6 @@ public class Document implements Comparable<Document> {
 
 
     public int getThreadId() {
-        if(documentHasBeenCleared)
-            throw new IllegalStateException("Document has already been cleared!");
-
         return threadId;
     }
 
@@ -490,7 +486,6 @@ public class Document implements Comparable<Document> {
      * Removes the activations of this document from the model again.
      */
     public void clearActivations() {
-        documentHasBeenCleared = true;
         if(threadId == null) {
             return;
         }
