43567bd852b804eababb07d6ae5724a3766fee4e
Kishoreganesh Sundararajan <ks524046@dal.ca>
2023-04-06 06:48:14 -0300

    Test : Testcases added for updateFrequencyForIandO from PatternSynapse.

---
+++ core/src/test/java/network/aika/PatternSynapseTest.java
@@ -0,0 +1,55 @@
+package network.aika;
+import network.aika.elements.synapses.PatternSynapse;
+import network.aika.sign.Sign;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PatternSynapseTest {
+
+
+    @Test
+    public void testUpdateFrequencyForIandO_bothActive_InitialState() {
+        PatternSynapse patternSynapse = new PatternSynapse();
+        patternSynapse.setFrequency(Sign.POS,Sign.POS,0.0);
+        patternSynapse.updateFrequencyForIandO(true,true);
+        assertEquals(1, (int) patternSynapse.getFrequency(Sign.POS,Sign.POS,0.0));
+    }
+    @Test
+    public void testUpdateFrequencyForIandO_bothActive_UpdatedState() {
+        PatternSynapse patternSynapse = new PatternSynapse();
+        patternSynapse.setFrequency(Sign.POS,Sign.POS,12.0);
+        patternSynapse.updateFrequencyForIandO(true,true);
+        assertEquals(13, (int) patternSynapse.getFrequency(Sign.POS,Sign.POS,0.0));
+    }
+
+    @Test
+    public void testUpdateFrequencyForIandO_onlyIActive_InitialState() {
+        PatternSynapse patternSynapse = new PatternSynapse();
+        patternSynapse.setFrequency(Sign.POS,Sign.NEG,0.0);
+        patternSynapse.updateFrequencyForIandO(true,false);
+        assertEquals(1, (int) patternSynapse.getFrequency(Sign.POS,Sign.NEG,0.0));
+    }
+
+    @Test
+    public void testUpdateFrequencyForIandO_onlyIActive_UpdatedState() {
+        PatternSynapse patternSynapse = new PatternSynapse();
+        patternSynapse.setFrequency(Sign.POS,Sign.NEG,8.0);
+        patternSynapse.updateFrequencyForIandO(true,false);
+        assertEquals(9, (int) patternSynapse.getFrequency(Sign.POS,Sign.NEG,0.0));
+    }
+
+    @Test
+    public void testUpdateFrequencyForIandO_onlyOActive_InitialState() {
+        PatternSynapse patternSynapse = new PatternSynapse();
+        patternSynapse.setFrequency(Sign.NEG,Sign.POS,0.0);
+        patternSynapse.updateFrequencyForIandO(false,true);
+        assertEquals(1, (int) patternSynapse.getFrequency(Sign.NEG,Sign.POS,0.0));
+    }
+    @Test
+    public void testUpdateFrequencyForIandO_onlyOActive_UpdatedState() {
+        PatternSynapse patternSynapse = new PatternSynapse();
+        patternSynapse.setFrequency(Sign.NEG,Sign.POS,6.0);
+        patternSynapse.updateFrequencyForIandO(false,true);
+        assertEquals(7, (int) patternSynapse.getFrequency(Sign.NEG,Sign.POS,0.0));
+    }
+}
