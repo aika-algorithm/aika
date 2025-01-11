52544f5c22af87517460db45c7d159b56ada8588
lukas.molzberger <lukas.molzberger@gmx.de>
2025-01-11 08:30:29 +0100

    Softmax fixes

--- fields/src/test/java/network/aika/fields/softmax/SoftmaxInputType.java
+++ fields/src/test/java/network/aika/fields/softmax/SoftmaxInputType.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package network.aika.fields.softmax;
 
 
@@ -12,7 +28,10 @@ import java.util.List;
 import static network.aika.fields.softmax.SoftmaxNormType.NORM_TO_INPUT;
 import static network.aika.fields.softmax.SoftmaxOutputType.CORRESPONDING_INPUT_LINK;
 
-
+/**
+ *
+ * @author Lukas Molzberger
+ */
 public class SoftmaxInputType extends Type<SoftmaxInputType, SoftmaxInputObj> {
 
     public static RelationOne<SoftmaxInputType, SoftmaxInputObj, SoftmaxNormType, SoftmaxNormObj> INPUT_TO_NORM = new RelationOne<>(SoftmaxInputObj::getNormObject, 0, "INPUT_TO_NORM");
--- fields/src/test/java/network/aika/fields/softmax/SoftmaxNormType.java
+++ fields/src/test/java/network/aika/fields/softmax/SoftmaxNormType.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package network.aika.fields.softmax;
 
 import network.aika.type.Type;
@@ -11,7 +27,10 @@ import java.util.List;
 import static network.aika.fields.softmax.SoftmaxInputType.INPUT_TO_NORM;
 import static network.aika.fields.softmax.SoftmaxOutputType.OUTPUT_TO_NORM;
 
-
+/**
+ *
+ * @author Lukas Molzberger
+ */
 public class SoftmaxNormType extends Type<SoftmaxNormType, SoftmaxNormObj> {
 
     public static RelationMany<SoftmaxNormType, SoftmaxNormObj, SoftmaxInputType, SoftmaxInputObj> NORM_TO_INPUT = new RelationMany<>(SoftmaxNormObj::getInputs, 0, "NORM_TO_INPUT");
