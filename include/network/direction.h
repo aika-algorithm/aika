e2ff8c882f0c41c845cda5f296297ca677e88721
lukas.molzberger <lukas.molzberger@gmx.de>
2025-05-16 15:28:27 +0200

    Work on direction

--- include/network/direction.h
+++ include/network/direction.h
@@ -1,7 +1,25 @@
 #ifndef NETWORK_DIRECTION_H
 #define NETWORK_DIRECTION_H
 
-// Use the Direction class from the fields folder
-#include "fields/direction.h"
+#include "network/model.h"
+#include "network/activation.h"
+#include "network/link.h"
+#include "network/neuron.h"
+#include "network/synapse.h"
+#include "network/bs_type.h"
+#include "network/transition.h"
 
-#endif // NETWORK_DIRECTION_H
+class Direction {
+public:
+    virtual ~Direction() = default;
+    virtual Direction* invert() = 0;
+    virtual Neuron* getNeuron(Model* m, Synapse* s) = 0;
+    virtual Activation* getActivation(Link* l) = 0;
+    virtual int getOrder() = 0;
+    virtual BSType* transition(BSType* s, Transition* t) = 0;
+    virtual void write(DataOutput* out) = 0;
+
+    static Direction* read(DataInput* in);
+};
+
+#endif // NETWORK_DIRECTION_H 
